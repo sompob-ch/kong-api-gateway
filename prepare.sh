9e7335a91b86b4fc246f0db094e77eaa52bc83c0
Sompob Chanintornpipat <sompob.chanintornpipat@gmail.com>
2024-07-10 16:42:20 +0700

    generate SSL

--- prepare.sh
+++ prepare.sh
@@ -1,11 +1,11 @@
+# generate SSL key and cert
+openssl req -x509 -nodes -newkey rsa:4096 -keyout ./ssl/key.pem -out ./ssl/cert.pem -days 365 -subj '/CN=localhost/'
+
 # start container
 KONG_DATABASE=postgres docker compose --profile database up --build --force-recreate
 
-# check kong proxy
-curl -i 'http://localhost:8000'
-
-# check kong admin
-curl -i 'http://localhost:8001'
-
 # clean up
 KONG_DATABASE=postgres docker compose --profile database down -v
+
+# test SSL
+curl -v --cacert ./ssl/cert.pem https://localhost:8443
