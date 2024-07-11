7d1f0c83175333fb812939878328889514d7bdf9
Sompob Chanintornpipat <sompob.chanintornpipat@gmail.com>
2024-07-11 10:06:21 +0700

    update doc

--- prepare.sh
+++ prepare.sh
@@ -1,11 +1,11 @@
-# generate SSL key and cert
-openssl req -x509 -nodes -newkey rsa:4096 -keyout ./ssl/key.pem -out ./ssl/cert.pem -days 365 -subj '/CN=localhost/'
-
 # start container
 KONG_DATABASE=postgres docker compose --profile database up --build --force-recreate
 
 # clean up
 KONG_DATABASE=postgres docker compose --profile database down -v
+
+# generate SSL key and cert
+openssl req -x509 -nodes -newkey rsa:4096 -keyout ./ssl/key.pem -out ./ssl/cert.pem -days 365 -subj '/CN=localhost/'
 
 # test SSL
 curl -v --cacert ./ssl/cert.pem https://localhost:8443
